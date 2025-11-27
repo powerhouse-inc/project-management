1654bf1cc94ef5fdf3dc78fd58350670fd570559
liberuum <liberuum@powerhouse.inc>
2025-11-27 16:09:20 +0100

    upgradae to 5.0.12

--- document-models/document-models.ts
+++ document-models/document-models.ts
@@ -1,4 +1,6 @@
 import type { DocumentModelModule } from "document-model";
 import { ScopeOfWork } from "./scope-of-work/module.js";
 
-export const documentModels: DocumentModelModule<any>[] = [ScopeOfWork];
+export const documentModels: DocumentModelModule<any>[] = [
+  ScopeOfWork,
+];
